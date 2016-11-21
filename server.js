2c44fe55fdfcd38896061d32c357bde984d6c300
Hyoungwook Kim <k@alina.ai>
2016-11-21 21:41:52 +0900

    API handlers.

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import 'app-module-path/register';
 import express from 'express';
+import morgan from 'morgan';
 
+import api from 'lib/api';
 import config from 'lib/config';
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api', api);
 
 app.listen(config.port);
 
