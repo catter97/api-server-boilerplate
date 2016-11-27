4eff5bdae91119acb4bd81d7aefe22be8af41932
khu <hukim99@gmail.com>
2016-11-27 19:18:57 +0900

    Organize files around features, not roles.

--- script/db.js
+++ script/db.js
@@ -1,19 +1,19 @@
-import * as models from 'lib/model';
+import { User } from 'lib/user';
 import { end, query } from 'lib/query';
 
-const creationOrder = [
-  models.User,
+const models = [
+  User,
 ];
 
 export const createAllTables = async () => { // eslint-disable-line import/prefer-default-export
-  for (let i = 0; i < creationOrder.length; i += 1) {
-    await query(creationOrder[i].create().ifNotExists());
+  for (let i = 0; i < models.length; i += 1) {
+    await query(models[i].create().ifNotExists());
   }
 };
 
 export const dropAllTables = async () => { // eslint-disable-line import/prefer-default-export
-  for (let i = creationOrder.length - 1; i >= 0; i -= 1) {
-    await query(creationOrder[i].drop().ifExists());
+  for (let i = models.length - 1; i >= 0; i -= 1) {
+    await query(models[i].drop().ifExists());
   }
 };
 
