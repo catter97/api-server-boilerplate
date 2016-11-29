281fb17ddf59fa1bc66709469dd7bd417d24c855
Hyoungwook Kim <k@alina.ai>
2016-11-29 18:26:40 +0900

    Test for users & login.

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 import 'app-module-path/register';
-import bodyParser from 'body-parser';
-import express from 'express';
-import session from 'express-session';
-import morgan from 'morgan';
-import passport from 'passport';
 
-import 'lib/auth';
-import api from 'lib/api';
+import app from 'lib/app';
 import config from 'lib/config';
-
-const app = express();
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
-}
-
-app.use(session({
-  secret: config.session.secret,
-  resave: false,
-  saveUninitialized: false,
-}));
-app.use(passport.initialize());
-app.use(bodyParser.json());
-app.use('/api', api);
 
 app.listen(config.port);
 
